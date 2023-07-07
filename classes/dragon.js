b5e31e111fa362d0118fc9cce1941ced130ec99b
StephanyKleen <stephanykleen@gmail.com>
2023-07-07 16:18:48 -0700

    Solve tests by creating classes, exporting and importing modules, and utilizing the ooo principles of inheritance to access parent class methods and properties

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,30 @@
 // Your code here
 
+
+class Dragon{
+  constructor(name, color){
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire(){
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+   static getDragons(...instances){
+    const dragonNames = [];
+    
+      instances.forEach( (instance) => {
+          dragonNames.push(instance.name)
+      })
+
+    return dragonNames;
+  }
+}
+
+
+
+module.exports = Dragon;
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,26 @@
 // Your code here
+const Dragon = require('../classes/dragon')
+
+class FriendlyDragon extends Dragon{
+    constructor(name, color, lifeGoals, friend){
+        super(name, color);
+        this.lifeGoals = lifeGoals;
+        this.friend = friend;
+    
+    }
+
+    hasLifeGoals(){
+       this.lifeGoals.forEach( (goal) => {
+         console.log(`${this.name} likes to ${goal}`)
+       })
+    }
+
+    helpsPeople(){
+        return `${this.name} helps their friend ${this.friend}`
+    }
+
+
+}
+
+
+module.exports = FriendlyDragon;
